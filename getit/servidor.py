a8ddf69d8dcd81e4660e98aaaebc1e04f7042a13
Arthur Belei <arthurbzg@al.insper.edu.br>
2025-02-05 20:18:06 -0300

    commit handout parte 3

--- getit/servidor.py
+++ getit/servidor.py
@@ -1,6 +1,14 @@
-from flask import Flask, request
+from flask import Flask, render_template_string, url_for
+from utils import load_data
+
 
 app = Flask(__name__)
+
+NOTE_TEMPLATE = '''  <li>
+    <h3>{title}</h3>
+    <p>{details}</p>
+  </li>
+'''
 
 RESPONSE_TEMPLATE = '''<!DOCTYPE html>
 <html>
@@ -10,53 +18,31 @@ RESPONSE_TEMPLATE = '''<!DOCTYPE html>
 </head>
 <body>
 
-<h1>Get-it</h1>
-<img src="img/logo-getit.png">
+<img src="{{{{ url_for('static', filename='img/logo-getit.png') }}}}">
 <p>Como o Post-it, mas com outro verbo</p>
 
-
 <ul>
-  <li>
-    <h3>Receita de miojo</h3>
-    <p>Bata com um martelo antes de abrir o pacote. Misture o tempero, coloque em uma vasilha e aproveite seu snack :)</p>
-  </li>
-  <li>
-    <h3>Pão doce</h3>
-    <p>Abra o pão e coloque o seu suco em pó favorito.</p>
-  </li>
-  <li>
-    <h3>Sorvete com cristais de leite</h3>
-    <p>Sirva o seu sorvete favorito em uma vasilha e jogue leite em cima.</p>
-  </li>
-  <li>
-    <h3>Iogurte natural</h3>
-    <p>Deixe o leite fora da geladeira (esse é mentira, não faça isso).</p>
-  </li>
-  <li>
-    <h3>Homer Simpson</h3>
-    <p>~( 8(|)</p>
-  </li>
-  <li>
-    <h3>Numero mágico</h3>
-    <p>142857</p>
-  </li>
-  <li>
-    <h3>Série da Fundação - Isaac Asimov</h3>
-    <p>É boa, leia.</p>
-  </li>
+{notes}
 </ul>
 
 </body>
 </html>
-
-
 '''
+
+# Configurando a pasta de arquivos estáticos
+app.static_folder = 'static'
+
 @app.route('/')
 def index():
-    print(request.method)
-    print(request.headers)
+    notes_li = [
+        NOTE_TEMPLATE.format(title=dados['titulo'], details=dados['detalhes'])
+        for dados in load_data('notes.json')
+    ]
+    notes = '\n'.join(notes_li)
 
-    return RESPONSE_TEMPLATE
+    response = RESPONSE_TEMPLATE.format(notes=notes)
+
+    return render_template_string(response)
 
 
 if __name__ == '__main__':
